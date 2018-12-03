38809aa1bd255320c83090ef53ad45baf439f8cd
alexthem8 <alexthem8@alexthem8-Inspiron-7559>
2018-12-03 13:33:59 -0500

    Updated Files

--- src/MED_Tester.cpp
+++ src/MED_Tester.cpp
@@ -9,34 +9,43 @@
 #include <iostream>       // std::cout
 #include <thread>         // std::thread
 #include <sstream>
+#include "../../crypto/includes/crypto.h"
+#include "../../crypto/includes/crypto_AES.h"
+#include "../../crypto/includes/HexHelper.h"
+#include "../../datastore/includes/data_store.h"
+#include "../../datastore/includes/data_store_file.h"
+#include "../../datastore/includes/string_data.h"
+#include "../../stringdatabase/includes/string_database.h"
 
 //I've provided some hints in the form of comments and commented out code
 //use them if you wish
 
-////global database object 
-//String_Database myGlobalCache;
+//global database object
+/**
+String_Database myGlobalCache;
 
-//bool testSerialization(const std::string &MYFILE1, const std::string &MYFILE2,
-//		Crypto *pCrypto) {
-//	DataStore_File myDataStore_File1(MYFILE1, pCrypto);
-//	myGlobalCache.save(&myDataStore_File1);
-//
-//	//clear cache
-//	myGlobalCache.clear();
-//	//	std::cout<<"Clearing Cache\n";
-//
-//	//load it
-//	myGlobalCache.load(&myDataStore_File1);
-//	//	std::cout<<"Loading from file "+ MYFILE1 +" \n";
-//
-//	//save to a different file
-//	DataStore_File myDataStore_File2(MYFILE2, pCrypto);
-//	myGlobalCache.save(&myDataStore_File2);
-//	//	std::cout<<"Saving to file "+ MYFILE2 +" \n";
+bool testSerialization(const std::string &MYFILE1, const std::string &MYFILE2,
+		Crypto *pCrypto) {
+	DataStore_File myDataStore_File1(MYFILE1, pCrypto);
+	myGlobalCache.save(&myDataStore_File1);
+
+	//clear cache
+	myGlobalCache.clear();
+		std::cout<<"Clearing Cache\n";
+
+	//load it
+	myGlobalCache.load(&myDataStore_File1);
+		std::cout<<"Loading from file "+ MYFILE1 +" \n";
+
+	//save to a different file
+	DataStore_File myDataStore_File2(MYFILE2, pCrypto);
+	myGlobalCache.save(&myDataStore_File2);
+		std::cout<<"Saving to file "+ MYFILE2 +" \n";
 
 	//I use my own objects here to compare the files
-//	return true;
-//}
+	return true;
+}
+*/
 
 int main() {
 
@@ -48,9 +57,14 @@ int main() {
 
 	//then I tested  serialization
 	//first without encryption, 
-	//testSerialization(NO_ENCRYPT_FILE1, NO_ENCRYPT_FILE2, 0);
+	/**
+	testSerialization("", "", 0);
 
 	//then with
-	//Crypto_AES myCrypto("I Like Rollos   ");
-	//testSerialization(ENCRYPT_FILE1, ENCRYPT_FILE2, &myCrypto);
+	std::string str = "I Like Rollos   ";
+	char *cstr = new char[str.length() + 1];
+	strcpy(cstr, str.c_str());
+	Crypto_AES myCrypto(cstr);
+	testSerialization("", "", &myCrypto);
+	*/
 }
